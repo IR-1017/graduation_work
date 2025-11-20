c4e6745fcc1bdf9f56309700a7ae27ed48ae4663
山崎郁弥 <yamazakiikuya@yamazakifumiyanoMacBook-Pro.local>
2025-11-20 23:48:13 +0900

    主にdatebase.ymlの編集とgemの追加追加/rubocop

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# アプリ共通のメール設定を行うベースクラス
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
